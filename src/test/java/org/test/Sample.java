4eb1728363e18c1757fd28a48b155d270f870fa1
dinesh-08-12 <dinesh.j.kmu.8@gmail.com>
2024-01-18 18:32:41 +0530

    GG

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import org.junit.Test;
+
+public class Sample {
+	
+	@Test
+	private void test() {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+
+	}
+
+}
